ea1dd7196745d305c88e1bc2751821415129ae83
Precious Ebube <precious.ebube@softescu.com>
2023-01-21 06:29:24 +0100

    testing

--- app/main.php
+++ app/main.php
@@ -13,8 +13,8 @@ $accept = socket_accept($sock); // Wait for first client
 
 while(true) {
  $buf = socket_read($accept, 2048, PHP_NORMAL_READ);
- $response = "+PONG\r\n";
- socket_write($accept, $response);
+ $response = "+PONG";
+ socket_write($accept, $response, strlen($response));
 }
 
 socket_close($accept);
