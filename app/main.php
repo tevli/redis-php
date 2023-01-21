aff5c179a0683ea9f850f417bdd3660665175335
Precious Ebube <precious.ebube@softescu.com>
2023-01-21 08:53:38 +0100

    k

--- app/main.php
+++ app/main.php
@@ -9,7 +9,7 @@ $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($sock, SOL_SOCKET, SO_REUSEPORT, 1);
 socket_bind($sock, "localhost", 6379);
 socket_listen($sock, 5);
-$accept = socket_accept($sock); // Wait for first client
+//$accept = socket_accept($sock); // Wait for first client
 
 $clients = array($sock);
 
