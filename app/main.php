5771cccbc61f8f05c6d8274bcb24b4715f57e32a
Precious Ebube <precious.ebube@softescu.com>
2023-01-22 18:25:47 +0100

    4

--- app/main.php
+++ app/main.php
@@ -1,15 +1,12 @@
 <?php
 error_reporting(E_ALL);
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
 echo "Logs from your program will appear here";
 
-// Uncomment this to pass the first stage
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_set_option($sock, SOL_SOCKET, SO_REUSEPORT, 1);
 socket_bind($sock, "localhost", 6379);
 socket_listen($sock, 5);
-//$accept = socket_accept($sock); // Wait for first client
 
 $clients = array($sock);
 
@@ -24,7 +21,6 @@ while(true) {
          unset($read[$key]);
    }
   foreach($read as $r) {
-   var_dump($r);
    socket_read($r,2048);
    socket_write($r, $response, strlen($response));
   }
