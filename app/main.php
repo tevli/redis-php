443f58310fa20f79d9afa93c457fa2933ef30460
Precious Ebube <precious.ebube@softescu.com>
2023-01-28 11:16:28 +0100

    r4

--- app/main.php
+++ app/main.php
@@ -49,7 +49,7 @@ function _handle($message,&$values=[]): string
                             var_dump('spl[4] is '.$spl[4]);
                             //the message comes with an expiry date.
                             $values[$spl[1]]['exp'] = is_numeric($spl[4]) ? $spl[4] : 0;
-                            $values[$spl[1]]['exp_time'] = time();
+                            $values[$spl[1]]['exp_time'] = milliseconds();
                         }
                         $values[$spl[1]]['value'] = $spl[2];
                         var_dump('setting   ');
@@ -61,8 +61,8 @@ function _handle($message,&$values=[]): string
                         if(isset($values[$spl[1]]['exp_time'])){
                             $exp_time = $values[$spl[1]]['exp_time'];
                             $exp = $values[$spl[1]]['exp'];
-                            print_r(['time'=>time(),'exp_time'=>$exp_time,'diff'=>(time() - $exp_time),'exp'=>$exp]);
-                            if((microtime() - $exp_time)>=$exp){
+                            print_r(['time'=>milliseconds(),'exp_time'=>$exp_time,'diff'=>(milliseconds() - $exp_time),'exp'=>$exp]);
+                            if((milliseconds() - $exp_time)>=$exp){
                                 return _resp_format(0);
                             }
                         }
@@ -153,5 +153,12 @@ function _unserialize($message){
        var_dump("main_words here is=>$main_words");
        return $main_words;
 //    $message = preg_replace('/[^A-Za-z0-9\-]/', '', $message);
+}
+
+
+function milliseconds(): int
+{
+    $mt = explode(' ', microtime());
+    return intval( $mt[1] * 1E3 ) + intval( round( $mt[0] * 1E3 ) );
 }
 ?>
