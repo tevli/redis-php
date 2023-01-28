92722039299983b7c7bd364b05e6731c1740bc53
Precious Ebube <precious.ebube@softescu.com>
2023-01-28 11:11:09 +0100

    wow

--- app/main.php
+++ app/main.php
@@ -62,7 +62,7 @@ function _handle($message,&$values=[]): string
                             $exp_time = $values[$spl[1]]['exp_time'];
                             $exp = $values[$spl[1]]['exp'];
                             print_r(['time'=>time(),'exp_time'=>$exp_time,'diff'=>(time() - $exp_time),'exp'=>$exp]);
-                            if((microtime() - $exp_time)>$exp){
+                            if((microtime() - $exp_time)>=$exp){
                                 return _resp_format(0);
                             }
                         }
